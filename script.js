b9ce95ab30b7f1242b9533d811f77c299c9384be
Moses Lee <leecmoses@gmail.com>
2022-05-02 14:50:54 -0400

    Generate new quote on button click

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 "use strict";
 
+const quoteContainer = document.querySelector("#quote-container");
+const quoteText = document.querySelector("#quote");
+const authorText = document.querySelector(".quote-author");
+const newQuoteBtn = document.querySelector("#new-quote");
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote(arr) {
   // Pick a randome quote from array
-  const quote = arr[Math.floor(Math.random() * arr.length)];
+  let { text: quote, author } = arr[Math.floor(Math.random() * arr.length)];
 
-  return quote;
+  // Check if Author field is blank and replace it with 'Unknown'
+  author = author ?? "Unknown";
+  authorText.textContent = author;
+
+  // Check Quote length to determine styling
+  quote.length > 120
+    ? quoteText.classList.add("long-quote")
+    : quoteText.classList.remove("long-quote");
+  quoteText.textContent = quote;
 }
 
 // Get Quotes from API
@@ -17,7 +30,8 @@ async function getQuotes() {
   try {
     const resp = await fetch(apiUrl);
     apiQuotes = await resp.json();
-    console.log(newQuote(apiQuotes));
+    console.log(apiQuotes);
+    newQuote(apiQuotes);
   } catch (error) {
     // Handle error here
   }
@@ -25,3 +39,8 @@ async function getQuotes() {
 
 // On load
 getQuotes();
+
+newQuoteBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  newQuote(apiQuotes);
+});
