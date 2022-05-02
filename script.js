eed6f55902effc92ceb2f0e05d49cedadb553f3f
Moses Lee <leecmoses@gmail.com>
2022-05-02 14:18:33 -0400

    Implement quote api

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote(arr) {
+  // Pick a randome quote from array
+  const quote = arr[Math.floor(Math.random() * arr.length)];
+
+  return quote;
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const resp = await fetch(apiUrl);
+    apiQuotes = await resp.json();
+    console.log(newQuote(apiQuotes));
+  } catch (error) {
+    // Handle error here
+  }
+}
+
+// On load
+getQuotes();
