63c65c649086539f50ce511fc7635a2318000919
Moses Lee <leecmoses@gmail.com>
2022-05-02 15:15:51 -0400

    Create loader

--- script.js
+++ script.js
@@ -5,27 +5,45 @@ const quoteText = document.querySelector("#quote");
 const authorText = document.querySelector(".quote-author");
 const twitterBtn = document.querySelector("#twitter");
 const newQuoteBtn = document.querySelector("#new-quote");
+const loader = document.querySelector("#loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function completed() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote(arr) {
+  loading();
   // Pick a random quote from array
   let { text: quote, author } = arr[Math.floor(Math.random() * arr.length)];
 
   // Check if Author field is blank and replace it with 'Unknown'
   author = author ?? "Unknown";
-  authorText.textContent = author;
 
   // Check Quote length to determine styling
   quote.length > 120
     ? quoteText.classList.add("long-quote")
     : quoteText.classList.remove("long-quote");
+
+  // Set Quote and Auther, Hide Loader
   quoteText.textContent = quote;
+  authorText.textContent = author;
+  completed();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
 
   try {
